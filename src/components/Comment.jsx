f6084f7e0368b6bf2cfcc1efa8749faa6bfbe5e0
Carlos Bordalo <carlosvitor6@hotmail.com>
2022-10-14 16:06:19 -0300

    Fix prevState on likeCount

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,20 @@
-import styles from './Comment.module.css'
+import styles from './Comment.module.css';
 
-import { ThumbsUp, Trash } from 'phosphor-react'
-import { Avatar } from './Avatar'
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
-    onDeleteComment(content)
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(prevState => {
+      return prevState + 1;
+    });
   }
 
   return (
@@ -31,12 +40,12 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
     </div>
-  )
+  );
 }
